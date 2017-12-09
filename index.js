a65adbdf27652d8be0ea94ffeaf56bb3abe2efe5
Artem Kaloev <kaloev.eltech@gmail.com>
2017-12-10 00:09:40 +0300

    updated webhook using built-in api solutions

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
 import TelegramBot from 'node-telegram-bot-api';
 import config from 'config';
-import Koa from 'koa';
-import Router from 'koa-router';
-import  bodyParser from 'koa-bodyparser';
 
 const TOKEN = config.get("token");
-const bot = new TelegramBot(TOKEN);
-
-bot.setWebHook(`${config.get("url")}/bot`);
-
-const app = new Koa();
-
-const router = new Router();
-router.post('/bot', ctx => {
-    const { body } = ctx.request;
-    bot.processUpdate(body);
-    ctx.status = 200;
+const bot = new TelegramBot(TOKEN, {
+    webHook: {
+        port: config.get("port"),
+        autoOpen: false
+    }
 });
-app.use(bodyParser());
-app.use(router.routes());
 
-const port = config.get("port");
-app.listen(port, () => {
-    console.log(`Listening on ${port}`);
-});
+bot.openWebHook();
+bot.setWebHook(`${config.get("url")}/bot${TOKEN}`);
 
 bot.on('message', msg => {
     const { chat: { id }} = msg;
@@ -34,10 +21,3 @@ bot.onText(/\/help (.+)/, (msg, [source, match]) => {
     const { chat: { id }} = msg;
     bot.sendMessage(id, match);
 });
-
-
-//cmd + d              ->        дубликат текущей строки
-//cmd + backSpace      ->        удаляет всю строку
-//alt + arrow-up       ->        выделяет следующий по размеру блок кода
-//alt + ЛКМ            ->        мультикурсор
-
